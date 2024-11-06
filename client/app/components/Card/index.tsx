ba4cb38e4db7981c82a017097006f0e02bb7dc03
LiviuCaciulatu <liviucaciulatu@gmail.com>
2024-11-06 09:46:33 +0200

    service-section (#5)

--- client/app/components/Card/index.tsx
+++ client/app/components/Card/index.tsx
@@ -1,18 +1,18 @@
-import { CardProps } from '@models/card'
-import React from 'react'
+import { CardProps } from '@models/card';
+import React from 'react';
+import style from './style.module.scss';
 
-const Card: React.FC<CardProps> = ({ name, description, icon }) => {
+const Card: React.FC<CardProps> = ({ name, description }) => {
     return (
-        <div>
-            <h3>
-                {name}
-            </h3>
-            {icon && <image></image>}
-            <div>
-                {description}
+        <div className={style.fullCard}>
+            <div className={style.cardText}>
+                <h3 className={style.cardName}>{name}</h3>
+                <div className={style.cardDescription}>{description}</div>
             </div>
             <hr />
+            <div className={style.arrow} />
         </div>
-    )
+    );
 }
-export default Card
+
+export default Card;
--- client/app/components/DisplayMenu/index.tsx
+++ client/app/components/DisplayMenu/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import style from './style.module.scss';
 import Card from '@components/Card';
 import RedirectButton from '@components/RedirectButton';
@@ -9,37 +9,56 @@ import { MenuItemProps } from '@models/menuItem';
 import { CardProps } from '@models/card';
 
 const DisplayMenu: React.FC<MenuProps> = ({ menuItems }) => {
+    const [activeMenuItem, setActiveMenuItem] = useState<MenuItemProps | null>(null);
 
-    const [activeMenuItem, setActiveMenuItem] = useState<MenuItemProps>(menuItems[0]);
-    const handleMenuChange=(index:number)=>{
+    useEffect(() => {
+        if (menuItems.length > 0) {
+            setActiveMenuItem(menuItems[0]);
+        }
+    }, [menuItems]);
+
+    const handleMenuChange = (index: number) => {
         setActiveMenuItem(menuItems[index]);
-    }
+    };
 
     return (
-
         <section className={style.section}>
             <div className={style.topSection}>
-                <h1>Services</h1>
+                <h1 className={style.servicesTitle}>Services</h1>
                 <RedirectButton text={"TEST"} path={"/"} />
             </div>
 
-            <div className={style.bottomSection}>   
-
+            <div className={style.bottomSection}>
                 <div className={style.leftSide}>
-                    {menuItems.map((menuItem: MenuItemProps, index: number) => <div className={`${style.itemsListChild}`}onClick={()=>handleMenuChange(index)} key={index}>{menuItem.name}</div>)}
+                    {menuItems.map((menuItem: MenuItemProps, index: number) => (
+                        <div
+                            className={`${style.itemsListChild} ${activeMenuItem === menuItem ? style.active : ''}`}
+                            onClick={() => handleMenuChange(index)}
+                            key={index}
+                        >
+                            {menuItem.name}
+                        </div>
+                    ))}
                 </div>
 
                 <div className={style.rightSide}>
-                    <h2 className={style.menuItemName}>{activeMenuItem.name}</h2>
-                    <div className={style.cardContainer}>
-                    {activeMenuItem.cards.map((card: CardProps, index: number) => (
-                        <Card key={index} {...card} />
-                    ))}
-                    </div>
+                    {activeMenuItem ? (
+                        <>
+                            <h2 className={style.menuItemName}>{activeMenuItem.name}</h2>
+                            <div className={style.cardContainer}>
+                                {activeMenuItem.cards.map((card: CardProps, index: number) => (
+                                    <Card key={index} {...card} />
+                                ))}
+                            </div>
+                        </>
+                    ) : (
+                        <p>Please select a service to see details.</p>
+                    )}
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default DisplayMenu
+
+export default DisplayMenu;
