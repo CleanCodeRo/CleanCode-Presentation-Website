8b12cc177f2a32bdc1218b4bc9c248f0ff5276ab
LiviuCaciulatu <liviucaciulatu@gmail.com>
2024-11-11 19:37:07 +0200

    comments removed

--- client/app/components/Dropdown/index.tsx
+++ client/app/components/Dropdown/index.tsx
@@ -1,20 +1,27 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import style from "./style.module.scss"
-import { DropdownProps } from '@models/dropdown';
-import {CSSTransition} from "react-transition-group";
+import React, { useState } from "react";
+import style from "./style.module.scss";
+import { DropdownProps } from "@models/dropdown";
+import { CSSTransition } from "react-transition-group";
 
-const Dropdown:React.FC<DropdownProps> = ({category}) => {
-const [activeMenu, setActiveMenu]=useState<string>("main");
+const Dropdown: React.FC<DropdownProps> = ({ category }) => {
+  const [activeMenu, setActiveMenu] = useState<string>("main");
   return (
     <div className={`${style.dropdown}`}>
-      <CSSTransition in={activeMenu==="main"} unmountOnExit timeout={500} classNames={`${style.menuPrimary}`}>
-      <ul className={`${style.bigItemsList}`}>
-        {category.map((value, index) => (
-          <li className={`${style.text}`} key={index}><a className={`${style.element}`}>{value.name}</a></li>
-        ))}
-      </ul>
+      <CSSTransition
+        in={activeMenu === "main"}
+        unmountOnExit
+        timeout={500}
+        classNames={`${style.menuPrimary}`}
+      >
+        <ul className={`${style.bigItemsList}`}>
+          {category.map((value, index) => (
+            <li className={`${style.text}`} key={index}>
+              <a className={`${style.element}`}>{value.name}</a>
+            </li>
+          ))}
+        </ul>
       </CSSTransition>
     </div>
   );
