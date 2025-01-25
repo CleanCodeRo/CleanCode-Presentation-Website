34b5b0a6b06bedf31bed05554b64fa69177c56b3
LiviuCaciulatu <liviucaciulatu@gmail.com>
2025-01-25 12:46:14 +0200

    about us hero section (#24)

--- client/app/AboutUs/page.tsx
+++ client/app/AboutUs/page.tsx
@@ -1,11 +1,15 @@
+// pages/AboutUs.tsx
+
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import style from './style.module.scss';
-import Head from 'next/head';
-import Navbar from '@components/Navbar';
-import Footer from '@components/Footer';
-import { ABOUT_US_SECTION } from '@constants/aboutUs';
+import React, { useEffect, useState } from "react";
+import style from "./style.module.scss";
+import Head from "next/head";
+import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
+import OurProcess from "@components/OurProcess";
+import MissionVision from "@components/MissionVision";
+import AboutUsHero from "@components/AboutUsHero";
 
 interface ProcessStep {
   id: number;
@@ -25,13 +29,13 @@ const AboutUs = () => {
 
   useEffect(() => {
     const fetchProcessData = async () => {
-      const response = await fetch('/assets/json/process.json');
+      const response = await fetch("/assets/json/process.json");
       const data = await response.json();
       setProcessData(data);
     };
 
     const fetchMissionData = async () => {
-      const response = await fetch('/assets/json/mission.json');
+      const response = await fetch("/assets/json/mission.json");
       const data = await response.json();
       setMissionData(data);
     };
@@ -40,79 +44,20 @@ const AboutUs = () => {
     fetchMissionData();
   }, []);
 
-  const [firstPart, secondPart] = ABOUT_US_SECTION.split(
-    "By offering personalised software and strategic insights,"
-  );
-
   return (
     <>
       <Navbar />
       <Head>
         <title>About Us</title>
-        <meta name="description" content="Learn more about our company and values." />
+        <meta
+          name="description"
+          content="Learn more about our company and values."
+        />
       </Head>
-      <div className={style.aboutUsContainer}>
-        <div className={style.aboutUsSection}>
-          <div className={style.aboutUsText}>
-            <p>
-              <strong>cleanCode Solutions</strong>
-              {firstPart.replace("cleanCode Solutions", "")}
-            </p>
-            <p>
-              By offering personalised software and strategic insights,
-              {secondPart}
-            </p>
-          </div>
-          <img src="/assets/svg/process.svg" alt="about us" className={style.aboutUsImage} />
-        </div>
-        <div className={style.ourProcess}>
-          <div className={style.ourProcessSubcontainer}>
-            <div className={style.ourProcessTitle}>Our Process</div>
-            <div className={style.lineWithDot}>
-              <div className={style.line}></div>
-              <div className={style.dot}></div>
-            </div>
-            <div className={style.ourProcessElements}>
-              {processData.map((step) => (
-                <div key={step.id} className={style.processCard}>
-                  <div className={style.cardTitle}>{step.name}</div>
-                  <div className={style.cardDetail}>{step.detail}</div>
-                </div>
-              ))}
-            </div>
-          </div>  
-        </div>
-
-        <div className={style.missionVision}>
-          <div className={style.missionVisionSubcontainer}>
-            <div className={style.missionVisionTitle}>Mission and Vision</div>
-            <div className={style.lineWithDotTwo}>
-              <div className={style.lineTwo}></div>
-              <div className={style.dotTwo}></div>
-            </div>
-            <div className={style.missionVisionElements}>
-              {missionData.map((step) => (
-                <div key={step.id} className={style.missionVisionCard}>
-                  <div className={style.missionVisionTitle}>{step.name}</div>
-                  <div className={style.missionVisionDescription}>
-                    {Array.isArray(step.detail) ? (
-                      <ul>
-                        {step.detail.map((item, index) => (
-                          <li key={index}>{item}</li>
-                        ))}
-                      </ul>
-                    ) : (
-                      <p>{step.detail}</p>
-                    )}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-
-      </div>
-      <Footer />
+        <AboutUsHero />
+        <OurProcess processData={processData} />
+        <MissionVision missionData={missionData} />
+        <Footer />
     </>
   );
 };
---
+++ client/app/components/AboutUsHero/index.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import style from "./style.module.scss";
+
+const AboutUsHero = () => {
+  return (
+    <div className={style.heroContainer}>
+      <div className={style.aboutUsSection}>
+        <h1 className={style.aboutUsTitle}>
+          Outstanding individually.{" "}
+          <span className={style.highlight}>Unstoppable together.</span>
+        </h1>
+        <img
+          src="/assets/svg/about-us-together.svg"
+          alt="about us"
+          className={style.aboutUsImage}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsHero;
