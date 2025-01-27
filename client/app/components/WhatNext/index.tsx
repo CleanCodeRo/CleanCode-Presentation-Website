e36a776889e58f37ae2f3e260f5579e99520dad7
LiviuCaciulatu <liviucaciulatu@gmail.com>
2025-01-27 07:39:02 +0200

    contact us page updated (#29)
    Co-authored-by: Horia Mitrica <horia.mitrica@gmail.com>

--- client/app/components/WhatNext/index.tsx
+++ client/app/components/WhatNext/index.tsx
@@ -7,21 +7,17 @@ import { WHAT_NEXT_TITLE, WHAT_NEXT_ELEMENTS } from "@constants/constants";
 const WhatNext = () => {
   return (
     <div className={style.whatNextContainer}>
-      <div className={style.backgroundCover}></div>
-
       <h2 className={style.title}>{WHAT_NEXT_TITLE}</h2>
 
-      <div className={style.elementsContainer}>
+      <div className={style.gridContainer}>
         {WHAT_NEXT_ELEMENTS.map((element, index) => (
-          <div key={index} className={style.element}>
-            <div className={style.numberContainer}>
+          <React.Fragment key={index}>
+            <div className={style.leftColumn}>
               <div className={style.number}>{index + 1}</div>
-              {index < WHAT_NEXT_ELEMENTS.length - 1 && (
-                <div className={style.line}></div>
-              )}
+              {index < WHAT_NEXT_ELEMENTS.length - 1 && <div className={style.line}></div>}
             </div>
-            <div className={style.text}>{element}</div>
-          </div>
+            <div className={style.rightColumn}>{element}</div>
+          </React.Fragment>
         ))}
       </div>
     </div>
