adabfb4c2bb34229360c38a529974555d52a236b
nu_cristi <cristian.dobree98@gmail.com>
2025-08-10 20:00:21 +0300

    sitemap, robots.txt

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
+  trailingSlash: false,
   webpack(config) {
     config.cache = false;
     config.module.rules.push({
